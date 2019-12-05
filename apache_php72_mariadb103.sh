d6b843609ffbebfcdcc64879373cfd0036be520b
林 <hayashi@site-lab.jp>
2019-12-05 23:42:55 +0900

    ファイル軽量化

--- apache_php72_mariadb103.sh
+++ apache_php72_mariadb103.sh
@@ -88,6 +88,8 @@ if [ -e /etc/redhat-release ]; then
         echo "htaccess有効化した状態のconfファイルを作成します"
         echo ""
 
+        sed -i -e "151d" /etc/httpd/conf/httpd.conf
+        sed -i -e "151i AllowOverride All" /etc/httpd/conf/httpd.conf
         sed -i -e "350i #バージョン非表示" /etc/httpd/conf/httpd.conf
         sed -i -e "351i ServerTokens ProductOnly" /etc/httpd/conf/httpd.conf
         sed -i -e "352i ServerSignature off \n" /etc/httpd/conf/httpd.conf
